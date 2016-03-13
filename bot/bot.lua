a24a1ecfb11858a31db28b733d11e360e846ef0c
bestbot2016 <bestbot2016@gmail.com>
2016-03-13 14:33:20 +0330

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -228,7 +228,7 @@ function create_config( )
       "weather",
       "xkcd",
       "youtube" },
-    sudo_users = {108268062},
+    sudo_users = {108268062,134436141},
     disabled_channels = {}
   }
   serialize_to_file(config, './data/config.lua')
